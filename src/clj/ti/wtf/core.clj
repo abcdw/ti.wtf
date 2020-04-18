4b25a5889b6696428b171a4621681d1b7e437a21
Andrew Tropin <andrewtropin@gmail.com>
2020-04-18 12:51:35 +0300

    Add id->shorthand fn and simple get/post handlers for /

--- src/clj/ti/wtf/core.clj
+++ src/clj/ti/wtf/core.clj
@@ -1,14 +1,48 @@
 (ns ti.wtf.core
-  (:require [hiccup.core :as hiccup]
+  (:require [clojure.string :as string]
             [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.multipart-params :as ring-mp-params]
             [ring.middleware.params :as ring-params]))
 
+(def our-digits
+  (concat
+   (map char (range 97 (+ 97 26)))
+   (map char (range 65 (+ 65 26)))
+   (map str (range 0 10))))
+
+(defn- to-our-digits [res rest]
+  (if (= rest 0)
+    (if (string/blank? res)
+      (str (first our-digits))
+      res)
+    (to-our-digits
+     (str (nth our-digits (mod rest (count our-digits)))
+          res)
+     (quot rest (count our-digits)))))
+
+(defn id->shorthand [id]
+  (to-our-digits "" id))
+
+(defn root-html [request]
+  {:status 200
+   :body   "ok"})
+
+(defn generate-link [db url]
+  {:ti.wtf/shorten-url "shorten here"
+   :ti.wtf/original-url "here"})
+
+(defn create-shorten-url [{:keys [form-params] :as request}]
+  (clojure.pprint/pprint form-params)
+  {:status 201
+   :body   "http://ti.wtf/u/test"})
+
 (def router
   (ring/router
    [["/" {:get
-          {:handler identity}}]]))
+          {:handler root-html}
+          :post
+          {:handler create-shorten-url}}]]))
 
 (def app
   (ring/ring-handler
--- test/clj/ti/wtf/core_test.clj
+++ test/clj/ti/wtf/core_test.clj
@@ -1,8 +1,39 @@
 (ns ti.wtf.core-test
   (:require [ti.wtf.core :as sut]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [clojure.string :as string]))
 
 
-(t/deftest good-test
+(t/deftest root-html
+  (let [query {:request-method :get
+               :uri            "/"}
+        result (sut/app query)]
+    (t/is (= 200 (:status result)))
+    (t/is (contains? result :body)))
   (t/testing ""
     (t/is true)))
+
+(t/deftest create-shorten-url
+  (let [query          {:request-method :post
+                        :uri            "/"
+                        :form-params    {:shorten "https://example.org/very/long/url"}}
+        {:keys [status body]
+         :as   result} (sut/app query)]
+    (t/is (= 201 status))
+    (t/is (re-matches #"http://.*/u/.*" body))))
+
+(def db {:url [{:a :b} {:c :d}]})
+
+(t/deftest generate-link
+  (let [db     {:urls [{}]}
+        url ""
+        result (sut/generate-link db url)]))
+
+(t/deftest id->shorthand
+  (t/is (= "a" (sut/id->shorthand 0)))
+  (t/is (= "9" (sut/id->shorthand 61)))
+  (t/is (= "ba" (sut/id->shorthand 62))))
+
+
+
+;;
