ba6b7d6a816465802ecd4fd2f632e5721c531251
Andrew Tropin <andrewtropin@gmail.com>
2020-04-20 12:34:45 +0300

    Remove t/ alias for test namespace

--- test/clj/ti/wtf/core_test.clj
+++ test/clj/ti/wtf/core_test.clj
@@ -1,38 +1,34 @@
 (ns ti.wtf.core-test
   (:require [ti.wtf.core :as sut]
-            [clojure.test :as t]
+            [clojure.test :refer :all]
             [clojure.string :as string]))
 
 
-(t/deftest root-html
+(deftest root-html
   (let [query {:request-method :get
                :uri            "/"}
         result (sut/app query)]
-    (t/is (= 200 (:status result)))
-    (t/is (contains? result :body)))
-  (t/testing ""
-    (t/is true)))
+    (is (= 200 (:status result)))
+    (is (contains? result :body))))
 
-(t/deftest create-shorten-url
+(deftest create-shorten-url
   (let [query          {:request-method :post
                         :uri            "/"
                         :form-params    {:shorten "https://example.org/very/long/url"}}
         {:keys [status body]
          :as   result} (sut/app query)]
-    (t/is (= 201 status))
-    (t/is (re-matches #"http://.*/u/.*" body))))
+    (is (= 200 status))
+    (is (re-matches #"http://.*/u/.*" body))))
 
-(def db {:url [{:a :b} {:c :d}]})
-
-(t/deftest generate-link
+(deftest generate-link
   (let [db     {:urls [{}]}
         url ""
         result (sut/generate-link db url)]))
 
-(t/deftest id->shorthand
-  (t/is (= "a" (sut/id->shorthand 0)))
-  (t/is (= "9" (sut/id->shorthand 61)))
-  (t/is (= "ba" (sut/id->shorthand 62))))
+(deftest id->shorthand
+  (is (= "a" (sut/id->shorthand 0)))
+  (is (= "9" (sut/id->shorthand 61)))
+  (is (= "ba" (sut/id->shorthand 62))))
 
 
 
